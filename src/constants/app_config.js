724cacbc65fea63a4ed6a2a1f4b8a1e430ad12ee
Chris Henrick <chrishenrick@gmail.com>
2017-01-20 18:50:33 -0800

    Create app config file with Carto table name look up & basemap url

---
+++ src/constants/app_config.js
@@ -0,0 +1,13 @@
+// CARTO table names lookup
+export const cartoTables = {
+  nyc_borough: 'nyc_borough',
+  nyc_city_council: 'nyc_city_council',
+  nyc_community_board: 'nyc_community_board',
+  nyc_neighborhood: 'nyc_neighborhood',
+  nyc_nypd_precinct: 'nyc_nypd_precinct',
+  nyc_zip_codes: 'nyc_zip_codes',
+  nyc_crashes: 'export2016_07'
+};
+
+export const basemapURL =
+  'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
