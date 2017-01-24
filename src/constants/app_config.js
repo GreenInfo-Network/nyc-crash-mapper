ca92bef25b65e5f4f72520f6d6817e3576913c5d
Chris Henrick <chrishenrick@gmail.com>
2017-01-24 13:37:54 -0800

    Update app config with cartoUser, fix values for cartoTables

--- src/constants/app_config.js
+++ src/constants/app_config.js
@@ -1,3 +1,6 @@
+// CARTO account name
+cartoUser = 'chekpeds';
+
 // CARTO table names lookup
 export const cartoTables = {
   nyc_borough: 'nyc_borough',
@@ -5,9 +8,10 @@ export const cartoTables = {
   nyc_community_board: 'nyc_community_board',
   nyc_neighborhood: 'nyc_neighborhood',
   nyc_nypd_precinct: 'nyc_nypd_precinct',
-  nyc_zip_codes: 'nyc_zip_codes',
+  nyc_zip_code: 'nyc_zip_code',
   nyc_crashes: 'export2016_07'
 };
 
+// basemap for Leaflet
 export const basemapURL =
   'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
