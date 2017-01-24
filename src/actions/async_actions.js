e4a83718e7bc73dac62bd36a3e03e0f6c33cafe3
Chris Henrick <chrishenrick@gmail.com>
2017-01-24 15:38:18 -0800

    Add async action for default sql query

--- src/actions/async_actions.js
+++ src/actions/async_actions.js
@@ -1,26 +1,35 @@
 import { polyfill } from 'es6-promise';
-polyfill();
 import fetch from 'isomorphic-fetch';
 import * as config from '../constants/app_config';
 import * as actions from '../actions/';
 import * as sql from '../constants/sql_queries';
 
-const requestCrashData = () => ({
+polyfill();
+
+const requestCrashStatsData = () => ({
   type: actions.CRASHES_ALL_REQUEST
 });
 
-const recieveCrashData = (json) => ({
+const recieveCrashStatsData = json => ({
   type: actions.CRASHES_ALL_SUCCESS,
   json
 });
 
-const receiveCrashDataErr = (error) => ({
+const receiveCrashStatsError = error => ({
   type: actions.CRASHES_ALL_ERROR,
   error
 });
 
-const fetchCrashData = () => ({
-  const query = sql.crashesByDate();
-});
+const fetchCrashStatsData = ({ startDate, endDate }) => {
+  const query = sql.statsDate({ startDate, endDate });
+  const url = `${config.sql_endpoint}q=${query}`;
+  return (dispatch) => {
+    dispatch(requestCrashStatsData());
+    return fetch(url)
+      .then(res => res.json())
+      .then(json => dispatch(recieveCrashStatsData(json.rows)))
+      .catch(error => dispatch(receiveCrashStatsError(error)));
+  };
+};
 
-export default fetchCrashData;
+export default fetchCrashStatsData;
