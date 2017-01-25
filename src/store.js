a03b995a51acaf473abec7cfbc8e2e266f1bf2ba
Chris Henrick <chrishenrick@gmail.com>
2017-01-24 18:25:31 -0800

    Apply filterByTypeMiddleware
    This enforces logic with the redux store so that when a FilterButton that has already
    been selected is clicked again by the user, the FilterByType will revert
    back to 'ALL'. This negates the requirement for 'select all' buttons for
    persona and harm in FilterByType.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,8 @@
 import { applyMiddleware, createStore, compose } from 'redux';
-import createLogger from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
 import { createResponsiveStoreEnhancer } from 'redux-responsive';
 
 import rootReducer from './reducers/';
-
-const middleware = [thunkMiddleware];
-
-if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
-  // redux-logger only works in a browser environment
-  middleware.push(createLogger());
-}
+import middleware from './middleware';
 
 export default function makeStore(initialState) {
   const store = createStore(
