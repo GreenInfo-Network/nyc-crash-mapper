d28d34b52a556194d7a87c5c12496004f5b9ec0a
Chris Henrick <chrishenrick@gmail.com>
2017-01-24 17:07:40 -0800

    Make sure to export all action creators from src/actions/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,5 @@
 export { fetchCrashData } from './async_actions';
 export { startDateChange, endDateChange } from './date_range_actions';
+export { filterByTypeHarm, filterByTypePersona } from './filter_by_type_actions';
+export filterByArea from './filter_by_area_actions';
+export filterByContributingFactor from './filter_contributing_factor_actions';
