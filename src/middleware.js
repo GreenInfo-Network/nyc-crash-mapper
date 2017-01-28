eea755af3035347dbe0809253f951446b1e6b190
Chris Henrick <chrishenrick@gmail.com>
2017-01-27 16:43:48 -0800

    Remove unused vars in middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,6 @@
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 
-import * as actions from './constants/action_types';
-import { filterByTypeHarm, filterByTypePersona } from './actions/';
-
 const middleware = [thunkMiddleware];
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
