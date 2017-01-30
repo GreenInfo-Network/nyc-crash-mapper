b69416af169465680dc906d141c20ef38611e87f
Chris Henrick <chrishenrick@gmail.com>
2017-01-29 22:02:24 -0800

    Pass store.filterType to LeafletMap to set & update cartoLayer

--- src/containers/LeafletMapConnected.js
+++ src/containers/LeafletMapConnected.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { dateStringFormatModel } from '../constants/api';
 import LeafletMap from '../components/LeafletMap/';
 
-const mapStateToProps = ({ dateRange }, ownProps) => {
+const mapStateToProps = ({ dateRange, filterType }, ownProps) => {
   const { startDate, endDate } = dateRange;
   const { location: { query } } = ownProps;
   const { lat, lng, zoom } = query;
@@ -13,6 +13,7 @@ const mapStateToProps = ({ dateRange }, ownProps) => {
     lng: lng ? Number(lng) : undefined,
     startDate: startDate.format(dateStringFormatModel),
     endDate: endDate.format(dateStringFormatModel),
+    filterType,
   };
 };
 
