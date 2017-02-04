d339a604f17a220f39c4ec05ccd64ba3d8d5cf01
Chris Henrick <chrishenrick@gmail.com>
2017-02-03 16:48:10 -0800

    Implement copy URL to clipboard in ShareURL component

--- src/components/Modal/ShareURL.js
+++ src/components/Modal/ShareURL.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 // TO DO: create dl button using URL & SQL API query, e.g.
 // https://username.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20table_name&format=CSV
-export default () => (
-  <div className="modal-about">
-    <p>The current map view & active filters will be shared via this URL.</p>
-    <p>TO DO...</p>
-  </div>
-);
+class ShareURL extends Component {
+  constructor() {
+    super();
+    this.state = {
+      copied: false,
+      value: window.location.href
+    };
+  }
+
+
+  render() {
+    const { value, copied } = this.state;
+
+    return (
+      <div className="modal-share-url">
+        <p>The current map view and any active filters may be shared using this URL:</p>
+        <input type="text" value={value} onFocus={e => e.target.select()} />
+        <CopyToClipboard text={value} onCopy={() => this.setState({ copied: true })}>
+          <button>Copy</button>
+        </CopyToClipboard>
+        <span className="copied">
+          { copied ? 'Copied!' : '' }
+        </span>
+      </div>
+    );
+  }
+}
+
+export default ShareURL;
