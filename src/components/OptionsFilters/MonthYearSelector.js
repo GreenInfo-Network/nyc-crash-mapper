b96464c4aa70b8b879788604526ff750de6a2206
Chris Henrick <chrishenrick@gmail.com>
2017-02-08 21:54:54 -0800

    Group label with Select elements in MonthYearSelector

--- src/components/OptionsFilters/MonthYearSelector.js
+++ src/components/OptionsFilters/MonthYearSelector.js
@@ -19,31 +19,35 @@ const MonthYearSelector = (props) => {
 
   return (
     <div className="month-year-selector">
-      <label htmlFor="filter-by-year-month">
-        {`${prefix} Month`}
-      </label>
-      <Select
-        className="month-selector"
-        name="filter-by-year-month"
-        options={monthOptions}
-        onChange={m => handleChange(curYear, m.value)}
-        matchPos="start"
-        ignoreCase
-        clearable={false}
-        value={curMonth}
-      />
-      <label htmlFor="filter-by-year-month">
-        {`${prefix} Year`}
-      </label>
-      <Select
-        className="year-selector"
-        name="filter-by-year-month"
-        options={yearOptions}
-        onChange={y => handleChange(y.value, curMonth)}
-        matchPos="start"
-        clearable={false}
-        value={curYear}
-      />
+      <div className="label-select-group">
+        <label htmlFor="filter-by-year-month">
+          {`${prefix} Month`}
+        </label>
+        <Select
+          className="month-selector"
+          name="filter-by-year-month"
+          options={monthOptions}
+          onChange={m => handleChange(curYear, m.value)}
+          matchPos="start"
+          ignoreCase
+          clearable={false}
+          value={curMonth}
+        />
+      </div>
+      <div className="label-select-group">
+        <label htmlFor="filter-by-year-month">
+          {`${prefix} Year`}
+        </label>
+        <Select
+          className="year-selector"
+          name="filter-by-year-month"
+          options={yearOptions}
+          onChange={y => handleChange(y.value, curMonth)}
+          matchPos="start"
+          clearable={false}
+          value={curYear}
+        />
+      </div>
     </div>
   );
 };
