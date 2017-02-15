24b1794cf77521788170f80d1d97f0a0f6de8b4c
Chris Henrick <chrishenrick@gmail.com>
2017-02-15 12:18:23 -0800

    Add chekpeds logo & hyperlink to header, closes #47

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,10 +4,17 @@ const AppHeader = (props) => {
   const { openModal } = props;
   return (
     <div className="app-header">
-      <h2 className="header-title">NYC Crash Mapper</h2>
-      <a onClick={() => openModal('about')}>
-        <p className="header-about">About</p>
-      </a>
+      <div className="header-logo-title">
+        <div className="logo-chekpeds">
+          <a href="http://chekpeds.com" target="blank" rel="noopener noreferrer">
+            <img src="img/checkpeds_logo@2x.jpg" />
+          </a>
+        </div>
+        <h2 className="header-title">NYC Crash Mapper</h2>
+      </div>
+      <p className="header-about">
+        <a onClick={() => openModal('about')}>About</a>
+      </p>
     </div>
   );
 };
