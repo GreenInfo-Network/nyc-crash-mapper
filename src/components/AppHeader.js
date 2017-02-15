ba85d894c366a6b8e524765800f43b5ae7af0cb9
Chris Henrick <chrishenrick@gmail.com>
2017-02-15 14:11:32 -0800

    Fix eslint error img-has-alt

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -7,7 +7,7 @@ const AppHeader = (props) => {
       <div className="header-logo-title">
         <div className="logo-chekpeds">
           <a href="http://chekpeds.com" target="blank" rel="noopener noreferrer">
-            <img src="img/checkpeds_logo@2x.jpg" />
+            <img alt="chekpeds logo" src="img/checkpeds_logo@2x.jpg" />
           </a>
         </div>
         <h2 className="header-title">NYC Crash Mapper</h2>
