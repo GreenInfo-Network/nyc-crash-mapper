32377bcd38f41ac74d56f4ac84f042b98d3b6095
Chris Henrick <chrishenrick@gmail.com>
2017-11-09 10:56:55 -0800

    menu component forms partial query string for charts view #62

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import qs from 'query-string';
 
 // Renders the nav menu items in the header
-const Menu = () => {
+const Menu = (props) => {
+  const { openModal, ...rest } = props;
+  const queryString = qs.stringify(rest);
+
   const hostname = process.env.NODE_ENV === 'production'
     ? 'greeninfo-network.github.io'
     : 'localhost:8889';
 
   const items = [
-    { type: '', value: 'map', label: 'Map' },
+    { type: null, value: 'map', label: 'Map' },
     { type: 'link', value: 'trend', label: 'Trend' },
     { type: 'link', value: 'compare', label: 'Compare' },
     { type: 'link', value: 'rank', label: 'Rank' },
-    { type: '', value: 'about', label: 'About' },
+    { type: 'modal', value: 'about', label: 'About' },
   ];
-  const chartView = 'map';
 
   const mapTypeToElement = (item) => {
-    const { type } = item;
-    const className = chartView === item.value ? 'active' : null;
+    const { label, type, value } = item;
 
     switch (type) {
       case 'link':
@@ -25,23 +27,29 @@ const Menu = () => {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href={`http://${hostname}?view=${item.value}`}
-            className={className}
+            href={`http://${hostname}?view=${value}&${queryString}`}
           >
-            {item.label}
+            {label}
           </a>
         );
 
-      // case 'view':
-      //   return (
-      //     <button className={className} onClick={() => handleViewClick(item.value)}>
-      //       {item.label}
-      //     </button>
-      //   );
+      case 'modal':
+        return (
+          <button onClick={() => openModal(value)}>
+            {label}
+          </button>
+        );
 
       // TO DO: implement "About"
       default:
-        return <button className={className} onClick={() => {}}>{item.label}</button>;
+        return (
+          <button
+            className={value === 'map' ? 'active' : null}
+            onClick={() => {}}
+          >
+            {item.label}
+          </button>
+        );
     }
   };
 
@@ -50,6 +58,27 @@ const Menu = () => {
       {items.map(item => <li key={item.label}>{mapTypeToElement(item)}</li>)}
     </ul>
   );
+};
+
+Menu.propTypes = {
+  openModal: PropTypes.func.isRequired,
+  p1start: PropTypes.string.isRequired,
+  p1end: PropTypes.string.isRequired,
+  geo: PropTypes.string.isRequired,
+  primary: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  pinj: PropTypes.bool.isRequired,
+  pfat: PropTypes.bool.isRequired,
+  cinj: PropTypes.bool.isRequired,
+  cfat: PropTypes.bool.isRequired,
+  minj: PropTypes.bool.isRequired,
+  mfat: PropTypes.bool.isRequired,
+};
+
+Menu.defaultProps = {
+  primary: null,
 };
 
 export default Menu;
