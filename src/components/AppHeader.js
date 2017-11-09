c267bac7d9b29ed9deb3a52de942a1efedc387de
Chris Henrick <chrishenrick@gmail.com>
2017-11-09 10:55:55 -0800

    promote menu to container component #62

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,23 +1,20 @@
 import React, { PropTypes } from 'react';
 
-import Menu from './Menu';
+import MenuConnected from '../containers/MenuConnected';
 
-const AppHeader = (props) => {
-  const { openModal } = props;
-  return (
-    <div className="app-header">
-      <div className="header-logo-title">
-        <div className="logo-chekpeds">
-          <a href="http://chekpeds.com" target="blank" rel="noopener noreferrer">
-            <img alt="chekpeds logo" src="img/checkpeds_logo@2x.jpg" />
-          </a>
-        </div>
-        <h2 className="header-title">NYC Crash Mapper</h2>
+const AppHeader = () => (
+  <div className="app-header">
+    <div className="header-logo-title">
+      <div className="logo-chekpeds">
+        <a href="http://chekpeds.com" target="blank" rel="noopener noreferrer">
+          <img alt="chekpeds logo" src="img/checkpeds_logo@2x.jpg" />
+        </a>
       </div>
-      <Menu openModal={openModal} />
+      <h2 className="header-title">NYC Crash Mapper</h2>
     </div>
-  );
-};
+    <MenuConnected />
+  </div>
+);
 
 AppHeader.propTypes = {
   openModal: PropTypes.func.isRequired,
